2c1aea9af89f2829c756ae72a4800c900a0a8605
Joey Biotti <joey@MacBook-Pro-2.local>
2017-09-20 12:46:16 -0500

    amended gitigore to allow js and css to commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Write your Javascript code.
+
+$(".isReadBox").change(function(){
+    console.log("this works")
+    $(this).val(true)
+    $(".isReadForm").submit();
+    console.log($(this).val());
+});
+
+
+
