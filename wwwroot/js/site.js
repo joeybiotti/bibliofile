4cf486e85622c1c75ab78321414a6cb4ad9c55ac
Joey Biotti <joey@MacBook-Pro-2.local>
2017-09-20 14:28:28 -0500

    all a tags now bootstrap btns, added jquery so nav fades on scroll

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,5 +7,18 @@ $(".isReadBox").change(function(){
     console.log($(this).val());
 });
 
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
 
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
